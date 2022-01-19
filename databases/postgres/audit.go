d05e20ad1d6a65158b05ffc8f26e9841f49a3322
wangminxiang <ryougi.nevermore@hotmail.com>
2022-01-19 23:26:42 +0800

    mod audit

--- databases/postgres/audit.go
+++ databases/postgres/audit.go
@@ -80,6 +80,78 @@ func tryFillAuditCreate(ctx fns.Context, rv reflect.Value, tab *table) (err erro
 	return
 }
 
+func tryFillAuditModify(ctx fns.Context, rv reflect.Value, tab *table) (err error) {
+	modifies := tab.findAuditModify()
+	if len(modifies) == 0 {
+		return
+	}
+	var modifyBY interface{}
+	modifyByStringTypeKind := false
+	var modifyByColumn *column
+	modifyAT := time.Time{}
+	var modifyAtColumn *column
+	for _, modify := range modifies {
+		if modify.isAmb() {
+			modifyByColumn = modify
+			modifyByField := rv.FieldByName(modifyByColumn.FieldName)
+			if modifyByField.Type().Kind() == reflect.String {
+				modifyByStringTypeKind = true
+			}
+			modifyBY = modifyByField.Interface()
+		}
+		if modify.isAmt() {
+			modifyAtColumn = modify
+			modifyAT = rv.FieldByName(modifyAtColumn.FieldName).Convert(reflect.TypeOf(modifyAT)).Interface().(time.Time)
+		}
+	}
+	if modifyByColumn != nil {
+		hasModifyByValue := false
+		if modifyByStringTypeKind {
+			modifyByString := modifyBY.(string)
+			if modifyByString == "" {
+				user := ctx.User()
+				if !user.Exists() && user.Id() != "" {
+					modifyByString = user.Id()
+					rv.FieldByName(modifyByColumn.FieldName).SetString(modifyByString)
+					hasModifyByValue = true
+				}
+			} else {
+				hasModifyByValue = true
+			}
+		} else {
+			modifyByInt := reflect.ValueOf(modifyBY).Int()
+			if modifyByInt <= 0 {
+				user := ctx.User()
+				if !user.Exists() && user.Id() != "" {
+					modifyByString := user.Id()
+					modifyByInt0, toIntErr := strconv.Atoi(modifyByString)
+					if toIntErr != nil {
+						err = fmt.Errorf("modify by type is int but type of user id in context is not int")
+						return
+					}
+					modifyByInt = int64(modifyByInt0)
+					rv.FieldByName(modifyByColumn.FieldName).SetInt(modifyByInt)
+					hasModifyByValue = true
+				}
+			} else {
+				hasModifyByValue = true
+			}
+		}
+		if !hasModifyByValue {
+			err = fmt.Errorf("modify by column value is needed")
+			return
+		}
+	}
+	if modifyAtColumn != nil {
+		if modifyAT.IsZero() {
+			modifyAT = time.Now()
+			modifyAtField := rv.FieldByName(modifyAtColumn.FieldName)
+			modifyAtField.Set(reflect.ValueOf(modifyAT).Convert(modifyAtField.Type()))
+		}
+	}
+	return
+}
+
 func tryFillAuditVersion(rv reflect.Value, tab *table) {
 	versionColumn := tab.findAuditVersion()
 	if versionColumn != nil {
