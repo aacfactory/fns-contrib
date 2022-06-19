83c28f88c60b58b36c1fdf41c925fd542a02be2b
wangminxiang <ryougi.nevermore@hotmail.com>
2022-06-19 12:55:23 +0800

    change sql to pg

--- authorizations/store/postgres/store.go
+++ authorizations/store/postgres/store.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/aacfactory/errors"
 	"github.com/aacfactory/fns-contrib/databases/postgres"
-	"github.com/aacfactory/fns-contrib/databases/sql"
 	"github.com/aacfactory/fns/service/builtin/authorizations"
 	"github.com/aacfactory/logs"
 	"strings"
@@ -94,7 +93,7 @@ func (store *Store) RemoveUserTokens(ctx context.Context, userId string) (err er
 		query = query + `"` + store.Schema + `".`
 	}
 	query = query + `"` + store.Table + `" WHERE "USER_ID" = $1`
-	_, _, execErr := sql.Execute(ctx, query, userId)
+	_, _, execErr := postgres.ExecuteContext(ctx, query, userId)
 	if execErr != nil {
 		err = errors.ServiceError("authorizations postgres store: remove user tokens failed").WithCause(execErr)
 		return
