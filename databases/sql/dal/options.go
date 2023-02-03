205b09d16457b094729d0595a6c34b136d679f50
wangminxiang <ryougi.nevermore@hotmail.com>
2023-02-03 15:26:43 +0800

    add eager load mode

--- databases/sql/dal/options.go
+++ databases/sql/dal/options.go
@@ -5,6 +5,27 @@ import (
 	"github.com/aacfactory/fns-contrib/databases/sql"
 )
 
-func WithDatabase(ctx context.Context, database string) context.Context {
+const (
+	eagerLoadCtxKey = "@fns_sql_dal_load_eager"
+)
+
+func Database(ctx context.Context, database string) context.Context {
 	return sql.WithOptions(ctx, sql.Database(database))
+}
+
+func EagerLoad(ctx context.Context) context.Context {
+	return context.WithValue(ctx, eagerLoadCtxKey, true)
+}
+
+func NotEagerLoad(ctx context.Context) context.Context {
+	return context.WithValue(ctx, eagerLoadCtxKey, false)
+}
+
+func isEagerLoadMode(ctx context.Context) (ok bool) {
+	v := ctx.Value(eagerLoadCtxKey)
+	if v == nil {
+		return
+	}
+	ok = v.(bool)
+	return
 }
