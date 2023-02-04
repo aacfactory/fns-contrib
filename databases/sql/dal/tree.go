bfa8313cf7a3a17e120b91d700c6bc817f642f52
wangminxiang <ryougi.nevermore@hotmail.com>
2023-02-04 08:32:48 +0800

    mode query tree model

--- databases/sql/dal/tree.go
+++ databases/sql/dal/tree.go
@@ -11,15 +11,29 @@ type keyable interface {
 		~float32 | ~float64 | ~string
 }
 
-func QueryTree[T TreeModel, N keyable](ctx context.Context, conditions *Conditions, rootNodeValues ...N) (result T, err errors.CodeError) {
-	ctx = NotEagerLoad(ctx)
-	// todo query
-
-	// todo map list to trees
+func QueryTree[T TreeModel, N keyable](ctx context.Context, conditions *Conditions, rootNodeValue N) (result T, err errors.CodeError) {
+	results, queryErr := queryTrees[T, N](ctx, conditions, rootNodeValue)
+	if queryErr != nil {
+		err = errors.ServiceError("dal: query tree failed").WithCause(queryErr)
+		return
+	}
+	if results == nil || len(results) == 0 {
+		return
+	}
+	result = results[rootNodeValue]
 	return
 }
 
 func QueryTrees[T TreeModel, N keyable](ctx context.Context, conditions *Conditions, rootNodeValues ...N) (results map[N]T, err errors.CodeError) {
+	results, err = queryTrees[T, N](ctx, conditions, rootNodeValues...)
+	if err != nil {
+		err = errors.ServiceError("dal: query trees failed").WithCause(err)
+		return
+	}
+	return
+}
+
+func queryTrees[T TreeModel, N keyable](ctx context.Context, conditions *Conditions, rootNodeValues ...N) (results map[N]T, err errors.CodeError) {
 	ctx = NotEagerLoad(ctx)
 	// todo query
 
