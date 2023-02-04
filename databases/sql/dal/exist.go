758843b4e537a348dc07ed7897e7f3de8195a6ee
wangminxiang <ryougi.nevermore@hotmail.com>
2023-02-04 08:32:15 +0800

    mod exist

--- databases/sql/dal/exist.go
+++ databases/sql/dal/exist.go
@@ -3,9 +3,28 @@ package dal
 import (
 	"context"
 	"github.com/aacfactory/errors"
+	"github.com/aacfactory/fns-contrib/databases/sql"
 )
 
 func Exist[T Model](ctx context.Context, conditions *Conditions) (has bool, err errors.CodeError) {
-	//m := new(T)
+	model := newModel[T]()
+	_, generator, getGeneratorErr := getModelQueryGenerator(ctx, model)
+	if getGeneratorErr != nil {
+		err = errors.Warning("dal: exist failed").WithCause(getGeneratorErr)
+		return
+	}
+	// generator
+	_, query, arguments, generateErr := generator.Exist(ctx, conditions)
+	if generateErr != nil {
+		err = errors.Warning("dal: exist failed").WithCause(generateErr)
+		return
+	}
+	// handle
+	rows, queryErr := sql.Query(ctx, query, arguments...)
+	if queryErr != nil {
+		err = errors.ServiceError("dal: exist failed").WithCause(queryErr)
+		return
+	}
+	has = !rows.Empty()
 	return
 }
