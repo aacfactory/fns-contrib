30606d8eaedee4e0d13af9e430418675c397dd8a
wangminxiang <ryougi.nevermore@hotmail.com>
2023-02-06 05:23:15 +0800

    mod tree

--- databases/sql/dal/tree.go
+++ databases/sql/dal/tree.go
@@ -3,6 +3,7 @@ package dal
 import (
 	"context"
 	"github.com/aacfactory/errors"
+	"reflect"
 )
 
 type keyable interface {
@@ -11,8 +12,8 @@ type keyable interface {
 		~float32 | ~float64 | ~string
 }
 
-func QueryTree[T TreeModel, N keyable](ctx context.Context, conditions *Conditions, rootNodeValue N) (result T, err errors.CodeError) {
-	results, queryErr := queryTrees[T, N](ctx, conditions, rootNodeValue)
+func QueryTree[T TreeModel, N keyable](ctx context.Context, conditions *Conditions, orders *Orders, rng *Range, rootNodeValue N) (result T, err errors.CodeError) {
+	results, queryErr := queryTrees[T, N](ctx, conditions, orders, rng, rootNodeValue)
 	if queryErr != nil {
 		err = errors.ServiceError("dal: query tree failed").WithCause(queryErr)
 		return
@@ -24,8 +25,8 @@ func QueryTree[T TreeModel, N keyable](ctx context.Context, conditions *Conditio
 	return
 }
 
-func QueryTrees[T TreeModel, N keyable](ctx context.Context, conditions *Conditions, rootNodeValues ...N) (results map[N]T, err errors.CodeError) {
-	results, err = queryTrees[T, N](ctx, conditions, rootNodeValues...)
+func QueryTrees[T TreeModel, N keyable](ctx context.Context, conditions *Conditions, orders *Orders, rng *Range, rootNodeValues ...N) (results map[N]T, err errors.CodeError) {
+	results, err = queryTrees[T, N](ctx, conditions, orders, rng, rootNodeValues...)
 	if err != nil {
 		err = errors.ServiceError("dal: query trees failed").WithCause(err)
 		return
@@ -33,10 +34,56 @@ func QueryTrees[T TreeModel, N keyable](ctx context.Context, conditions *Conditi
 	return
 }
 
-func queryTrees[T TreeModel, N keyable](ctx context.Context, conditions *Conditions, rootNodeValues ...N) (results map[N]T, err errors.CodeError) {
+func queryTrees[T TreeModel, N keyable](ctx context.Context, conditions *Conditions, orders *Orders, rng *Range, rootNodeValues ...N) (results map[N]T, err errors.CodeError) {
+	if rootNodeValues == nil || len(rootNodeValues) == 0 {
+		err = errors.Warning("root node values are required")
+		return
+	}
 	ctx = NotEagerLoad(ctx)
 	// todo query
+	list, queryErr := QueryWithRange[T](ctx, conditions, orders, rng)
+	if queryErr != nil {
+		err = queryErr
+		return
+	}
+	results, err = mapListToTrees[T, N](list, rootNodeValues)
+	return
+}
 
-	// todo map list to trees
+func mapListToTrees[T TreeModel, N keyable](list []T, rootNodeValues []N) (nodes map[N]T, err errors.CodeError) {
+	nodes = make(map[N]T)
+
+	for _, rootNodeValue := range rootNodeValues {
+		node, mapErr := mapListToTree[T, N](list, rootNodeValue)
+		if mapErr != nil {
+			err = mapErr
+			return
+		}
+		if node == nil {
+			continue
+		}
+		nodes[rootNodeValue] = node
+	}
+	return
+}
+
+func mapListToTree[T TreeModel, N keyable](list []T, rootNodeValue N) (node T, err errors.CodeError) {
+	if list == nil || len(list) == 0 {
+		return
+	}
+	currentFieldName, parentFieldName, childrenFieldName := list[0].NodeReferenceFields()
+	for _, model := range list {
+		rv := reflect.ValueOf(model).Elem()
+		currentFieldValue, currentFieldValueTypeOk := rv.FieldByName(currentFieldName).Interface().(N)
+		if !currentFieldValueTypeOk {
+			err = errors.Warning("tree reference field value type is not matched").WithMeta("field", currentFieldName)
+			return
+		}
+		if rootNodeValue != currentFieldValue {
+			continue
+		}
+		// parent
+
+	}
 	return
 }
