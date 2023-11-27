da26ec08c9f6263bc1e04fd2ab6156b0469aecf4
wangminxiang <ryougi.nevermore@hotmail.com>
2023-11-28 04:26:10 +0800

    mod

--- databases/sql/dac/insert.go
+++ databases/sql/dac/insert.go
@@ -1,10 +1,37 @@
 package dac
 
 import (
+	"github.com/aacfactory/fns-contrib/databases/sql/dac/conditions"
 	"github.com/aacfactory/fns/context"
 )
 
-func Insert[T Table](ctx context.Context, entry T) (v T, err error) {
+type InsertOptions struct {
+	saveMode bool
+	notExist conditions.QueryExpr
+	exist    conditions.QueryExpr
+}
+
+type InsertOption func(options *InsertOptions)
+
+func InsertOrUpdate() InsertOption {
+	return func(options *InsertOptions) {
+		options.saveMode = true
+	}
+}
+
+func InsertWhenExist(src conditions.QueryExpr) InsertOption {
+	return func(options *InsertOptions) {
+		options.exist = src
+	}
+}
+
+func InsertWhenNotExist(src conditions.QueryExpr) InsertOption {
+	return func(options *InsertOptions) {
+		options.notExist = src
+	}
+}
+
+func Insert[T Table](ctx context.Context, entry T, options ...InsertOption) (v T, err error) {
 
 	return
 }
