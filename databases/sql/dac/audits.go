d975ac8b7aa611656bdae586b2bcea5a03f0e5a0
wangminxiang <ryougi.nevermore@hotmail.com>
2023-12-01 18:06:35 +0800

    mod

--- databases/sql/dac/audits.go
+++ databases/sql/dac/audits.go
@@ -3,30 +3,30 @@ package dac
 import "time"
 
 type AuditCreation[Id ~string | ~int64] struct {
-	CreateBY Id        `column:"CREATE_BY,ACB"`
-	CreateAT time.Time `column:"CREATE_AT,ACT"`
+	CreateBY Id        `column:"CREATE_BY,ACB" json:"createBY"`
+	CreateAT time.Time `column:"CREATE_AT,ACT" json:"createAT"`
 }
 
 type AuditModification[Id ~string | ~int64] struct {
-	ModifyBY Id        `column:"MODIFY_BY,AMB"`
-	ModifyAT time.Time `column:"MODIFY_AT,AMT"`
+	ModifyBY Id        `column:"MODIFY_BY,AMB" json:"modifyBY"`
+	ModifyAT time.Time `column:"MODIFY_AT,AMT" json:"modifyAT"`
 }
 
 type AuditDelete[Id ~string | ~int64] struct {
-	DeleteBY Id        `column:"DELETE_BY,ADB"`
-	DeleteAT time.Time `column:"DELETE_AT,ADT"`
+	DeleteBY Id        `column:"DELETE_BY,ADB" json:"deleteBY"`
+	DeleteAT time.Time `column:"DELETE_AT,ADT" json:"deleteAT"`
 }
 
 type AuditVersion struct {
-	Version int64 `column:"VERSION,AOL"`
+	Version int64 `column:"VERSION,AOL" json:"version"`
 }
 
 type IncrPk struct {
-	Id int64 `column:"ID,PK,INCR"`
+	Id int64 `column:"ID,PK,INCR" json:"id"`
 }
 
 type UID struct {
-	Id string `column:"ID,PK"`
+	Id string `column:"ID,PK" json:"id"`
 }
 
 type Audit struct {
