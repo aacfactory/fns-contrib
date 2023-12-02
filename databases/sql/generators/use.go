f115d54692687490b17be56088e87c1874c590f3
wangminxiang <ryougi.nevermore@hotmail.com>
2023-12-02 16:26:40 +0800

    mod

--- databases/sql/generators/use.go
+++ databases/sql/generators/use.go
@@ -2,6 +2,8 @@ package generators
 
 import (
 	"context"
+	"fmt"
+	"github.com/aacfactory/errors"
 	"github.com/aacfactory/gcg"
 )
 
@@ -15,6 +17,20 @@ func (writer *UseWriter) Annotation() (annotation string) {
 }
 
 func (writer *UseWriter) HandleBefore(ctx context.Context, params []string, hasFnParam bool, hasFnResult bool) (code gcg.Code, err error) {
+	paramsLen := len(params)
+	if paramsLen != 1 {
+		err = errors.Warning("sql: generate use code failed").WithCause(fmt.Errorf("invalid annotation params"))
+		return
+	}
+	name := params[0]
+
+	stmt := gcg.Statements()
+	stmt.Tab().Token(fmt.Sprintf("sql.Use(ctx, bytex.FromString(\"%s\"))", name),
+		gcg.NewPackage("github.com/aacfactory/fns/commons/bytex"),
+		gcg.NewPackage("github.com/aacfactory/fns-contrib/databases/sql"),
+	).Line()
+
+	code = stmt
 	return
 }
 
