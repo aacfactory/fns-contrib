b85dc31e80f8170cb41b00aff8db714e57d38558
wangminxiang <ryougi.nevermore@hotmail.com>
2023-12-04 19:18:25 +0800

    mod

--- databases/sql/types.go
+++ databases/sql/types.go
@@ -41,38 +41,199 @@ type Scanner interface {
 	sql.Scanner
 	driver.Valuer
 	json.Marshaler
+	json.Unmarshaler
 }
 
 type NullString struct {
 	sql.NullString
 }
 
+func (n NullString) MarshalJSON() (p []byte, err error) {
+	if n.Valid {
+		p, err = json.Marshal(n.String)
+	}
+	return
+}
+
+func (n *NullString) UnmarshalJSON(p []byte) error {
+	if len(p) == 0 {
+		n.Valid = false
+		return nil
+	}
+	err := json.Unmarshal(p, &n.String)
+	if err != nil {
+		return err
+	}
+	n.Valid = true
+	return nil
+}
+
 type NullBool struct {
 	sql.NullBool
+}
+
+func (n NullBool) MarshalJSON() (p []byte, err error) {
+	if n.Valid {
+		p, err = json.Marshal(n.Bool)
+	}
+	return
+}
+
+func (n *NullBool) UnmarshalJSON(p []byte) error {
+	if len(p) == 0 {
+		n.Valid = false
+		return nil
+	}
+	err := json.Unmarshal(p, &n.Bool)
+	if err != nil {
+		return err
+	}
+	n.Valid = true
+	return nil
 }
 
 type NullInt16 struct {
 	sql.NullInt16
 }
 
+func (n NullInt16) MarshalJSON() (p []byte, err error) {
+	if n.Valid {
+		p, err = json.Marshal(n.Int16)
+	}
+	return
+}
+
+func (n *NullInt16) UnmarshalJSON(p []byte) error {
+	if len(p) == 0 {
+		n.Valid = false
+		return nil
+	}
+	err := json.Unmarshal(p, &n.Int16)
+	if err != nil {
+		return err
+	}
+	n.Valid = true
+	return nil
+}
+
 type NullInt32 struct {
 	sql.NullInt32
+}
+
+func (n NullInt32) MarshalJSON() (p []byte, err error) {
+	if n.Valid {
+		p, err = json.Marshal(n.Int32)
+	}
+	return
+}
+
+func (n *NullInt32) UnmarshalJSON(p []byte) error {
+	if len(p) == 0 {
+		n.Valid = false
+		return nil
+	}
+	err := json.Unmarshal(p, &n.Int32)
+	if err != nil {
+		return err
+	}
+	n.Valid = true
+	return nil
 }
 
 type NullInt64 struct {
 	sql.NullInt64
 }
 
+func (n NullInt64) MarshalJSON() (p []byte, err error) {
+	if n.Valid {
+		p, err = json.Marshal(n.Int64)
+	}
+	return
+}
+
+func (n *NullInt64) UnmarshalJSON(p []byte) error {
+	if len(p) == 0 {
+		n.Valid = false
+		return nil
+	}
+	err := json.Unmarshal(p, &n.Int64)
+	if err != nil {
+		return err
+	}
+	n.Valid = true
+	return nil
+}
+
 type NullFloat64 struct {
 	sql.NullFloat64
+}
+
+func (n NullFloat64) MarshalJSON() (p []byte, err error) {
+	if n.Valid {
+		p, err = json.Marshal(n.Float64)
+	}
+	return
+}
+
+func (n *NullFloat64) UnmarshalJSON(p []byte) error {
+	if len(p) == 0 {
+		n.Valid = false
+		return nil
+	}
+	err := json.Unmarshal(p, &n.Float64)
+	if err != nil {
+		return err
+	}
+	n.Valid = true
+	return nil
 }
 
 type NullByte struct {
 	sql.NullByte
 }
 
+func (n NullByte) MarshalJSON() (p []byte, err error) {
+	if n.Valid {
+		p, err = json.Marshal(n.Byte)
+	}
+	return
+}
+
+func (n *NullByte) UnmarshalJSON(p []byte) error {
+	if len(p) == 0 {
+		n.Valid = false
+		return nil
+	}
+	err := json.Unmarshal(p, &n.Byte)
+	if err != nil {
+		return err
+	}
+	n.Valid = true
+	return nil
+}
+
 type NullTime struct {
 	sql.NullTime
+}
+
+func (n NullTime) MarshalJSON() (p []byte, err error) {
+	if n.Valid {
+		p, err = json.Marshal(n.Time)
+	}
+	return
+}
+
+func (n *NullTime) UnmarshalJSON(p []byte) error {
+	if len(p) == 0 {
+		n.Valid = false
+		return nil
+	}
+	err := json.Unmarshal(p, &n.Time)
+	if err != nil {
+		return err
+	}
+	n.Valid = true
+	return nil
 }
 
 type NullJson[E any] struct {
@@ -170,7 +331,28 @@ func (n NullBytes) Value() (driver.Value, error) {
 
 func (n NullBytes) MarshalJSON() ([]byte, error) {
 	if n.Valid {
+		if json.Validate(n.Bytes) {
+			return n.Bytes, nil
+		}
 		return json.Marshal(n.Bytes)
 	}
 	return nil, nil
+}
+
+func (n *NullBytes) UnmarshalJSON(p []byte) error {
+	if len(p) == 0 {
+		n.Valid = false
+		return nil
+	}
+	if json.Validate(p) {
+		n.Bytes = p
+		n.Valid = true
+		return nil
+	}
+	err := json.Unmarshal(p, &n.Bytes)
+	if err != nil {
+		return err
+	}
+	n.Valid = true
+	return nil
 }
