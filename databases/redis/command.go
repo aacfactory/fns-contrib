01a25b12a07dfcc2167a8188a2430f7a9cf626cb
wangminxiang <ryougi.nevermore@hotmail.com>
2023-12-12 00:30:32 +0800

    mod

--- databases/redis/command.go
+++ databases/redis/command.go
@@ -1,4 +1,36 @@
 package redis
 
-type Command interface {
+import "github.com/redis/rueidis"
+
+const (
+	getCN = "GET"
+	setCN = "SET"
+)
+
+type Command struct {
+	Name   string   `json:"name"`
+	Params []string `json:"params"`
+}
+
+func (cmd *Command) as(client rueidis.Client) (v rueidis.Completed) {
+	switch cmd.Name {
+	case getCN:
+		v = client.B().Get().Key(cmd.Params[0]).Build()
+		break
+	case setCN:
+		break
+	default:
+		tokens := make([]string, 0, len(cmd.Params)+1)
+		tokens = append(tokens, cmd.Name)
+		for _, param := range cmd.Params {
+			tokens = append(tokens, param)
+		}
+		v = client.B().Arbitrary(tokens...).Build()
+		break
+	}
+	return
+}
+
+type IncompleteCommand interface {
+	Build() (cmd Command)
 }
--- databases/redis/fn.go
+++ databases/redis/fn.go
@@ -1,6 +1,7 @@
 package redis
 
 import (
+	"github.com/aacfactory/errors"
 	"github.com/aacfactory/fns/services"
 	"github.com/redis/rueidis"
 )
@@ -22,6 +23,35 @@ func (handler *commandHandler) Readonly() bool {
 }
 
 func (handler *commandHandler) Handle(ctx services.Request) (v any, err error) {
-
+	commands := make([]Command, 0, 1)
+	paramErr := ctx.Param().TransformTo(&commands)
+	if paramErr != nil {
+		err = errors.Warning("redis: parse param failed").WithCause(paramErr)
+		return
+	}
+	commandsLen := len(commands)
+	switch commandsLen {
+	case 0:
+		err = errors.Warning("redis: invalid param")
+		break
+	case 1:
+		resp := handler.client.Do(ctx, commands[0].as(handler.client))
+		results := make([]Result, commandsLen)
+		results[0] = newResult(resp)
+		v = results
+		break
+	default:
+		completes := make([]rueidis.Completed, commandsLen)
+		for i, command := range commands {
+			completes[i] = command.as(handler.client)
+		}
+		resp := handler.client.DoMulti(ctx, completes...)
+		results := make([]Result, commandsLen)
+		for i, redisResult := range resp {
+			results[i] = newResult(redisResult)
+		}
+		v = results
+		break
+	}
 	return
 }
