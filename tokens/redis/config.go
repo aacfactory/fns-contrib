24b185adddcdf983b2ca637eae8f59c2cad3a4d4
wangminxiang <ryougi.nevermore@hotmail.com>
2023-12-14 19:39:20 +0800

    mod

--- tokens/redis/config.go
+++
@@ -1,6 +0,0 @@
-package redis
-
-type Config struct {
-	Database  string `json:"database" yaml:"database"`
-	KeyPrefix string `json:"keyPrefix" yaml:"keyPrefix"`
-}
--- tokens/sql/config.go
+++
@@ -1,7 +0,0 @@
-package sql
-
-type Config struct {
-	Database string `json:"database" yaml:"database"`
-	Schema   string `json:"schema" yaml:"schema"`
-	Table    string `json:"table" yaml:"table"`
-}
